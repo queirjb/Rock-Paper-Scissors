e9b005809de1d3d625e164f33ba8506a321d04ea
Cambron Deatherage <ctd1077@gmail.com>
2018-09-19 20:13:43 -0600

    Verison 2

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
-Read Me
+# Project Title
+
+Rock, Paper, Scissors Project for Udacity Project
+
+## Getting Started
+
+Download and have fun!
+
+### Prerequisites
+
+A computer
+
+```
+Desktop, laptop, etc...
+```
+
+### Installing
+
+A step by step series of examples that tell you how to get a development env running
+
+Say what the step will be
+
+```
+Give the example
+```
+
+And repeat
+
+```
+until finished
+```
+
+End with an example of getting some data out of the system or using it for a little demo
+
+## Running the tests
+
+Explain how to run the automated tests for this system
+
+### Break down into end to end tests
+
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+### And coding style tests
+
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+## Deployment
+
+Add additional notes about how to deploy this on a live system
+
+## Built With
+
+* Python in Atom
+
+## Contributing
+
+Just me
+
+## Authors
+
+* **Cambron Deatherage** - *Initial work* -
+https://github.com/ctd1077
+
+## License
+
+N/A
+
+## Acknowledgments
+
+* Hat tip to Udacity
+* Classmates for help along the way
--- rps.py
+++ rps.py
@@ -1,40 +1,39 @@
-## My Rock, Paper, Scissor Project
-## for Udacity Into to Programming class
-## Written by: Cambron Deatherage
-##Rules:
-##Rock beats scissors
-##Scissors beats paper
-##Paper beats rock
-
+# My Rock, Paper, Scissor Project
+# for Udacity Into to Programming class
+# Written by: Cambron Deatherage
+# Rules:
+# Rock beats scissors
+# Scissors beats paper
+# Paper beats rock
 import random
 
 moves = ['rock', 'paper', 'scissors']
 
+
 class Player():
+
     def __init__(self):
+
         self.score = 0
 
     def move(self):
+
         return moves[0]
 
     def learn(self, learn_move):
 
         pass
-## Parent Player class
-
-
+# Parent Player class
 
 
 class RandomPlayer(Player):
     def move(self):
         throw = random.choice(moves)
         return (throw)
-## This class selects a random move choice
+# This class selects a random move choice
 
 
-
-
-class  ReflectPlayer(Player):
+class ReflectPlayer(Player):
 
     def __init__(self):
 
@@ -42,17 +41,16 @@ class  ReflectPlayer(Player):
         self.learn_move = None
 
     def move(self):
-        if self.learn_move == None:
-          throw = moves[0]                      #  First move is always rock
+        if self.learn_move is None:
+            throw = moves[0]                      # First move is always rock
         else:
-          throw = self.learn_move               #  next move is humanplayers previous move
-        return (throw)
+            throw = self.learn_move               # next move is humanplayers
+            return (throw)                        # previous move
 
     def learn(self, learn_move):
 
         self.learn_move = learn_move
-## This Class reflects the human players 1st and 2nd moves
-
+# This Class reflects the human players 1st and 2nd moves
 
 
 class Cycles(Player):
@@ -65,18 +63,16 @@ class Cycles(Player):
     def move(self):
         throw = None
         if self.step == 0:
-          throw = moves[0]
-          self.step = self.step +1
+            throw = moves[0]
+            self.step = self.step + 1
         elif self.step == 1:
-          throw = moves[1]
-          self.step = self.step +1
+            throw = moves[1]
+            self.step = self.step + 1
         else:
-          throw = moves[2]
-          self.step = self.step +1
+            throw = moves[2]
+            self.step = self.step + 1
         return throw
-## This Class cycles through the moves list starting at rock
-
-
+# This Class cycles through the moves list starting at rock
 
 
 class HumanPlayer(Player):
@@ -86,25 +82,39 @@ class HumanPlayer(Player):
         throw = input('rock, paper, scissors? >')
 
         while throw != 'rock'and throw != 'paper'and throw != 'scissors':
-          print('Sorry try again')
-          throw = input('rock, paper, scissors? >')
+            print('Sorry try again')
+            throw = input('rock, paper, scissors? >')
         return (throw)
-## This class requests a human player to make a selection
+# This class requests a human player to make a selection
 
 
 class Game():
 
-    def __init__(self):
+    def __init__(self, p2):
         self.p1 = HumanPlayer()
-        self.p2 = RandomPlayer()
-
-
+        self.p2 = p2
 
     def play_game(self):
+
         print("Rock Paper Scissors, Go!")
         for round in range(3):
-          print (f"Round {round}:")
-          self.play_round()
+            print (f"Round {round}:")
+            self.play_round()
+        if self.p1.score > self.p2.score:
+            print('Player 1 won!')
+        elif self.p1.score < self.p2.score:
+            print('Player 2 won!')
+        else:
+            print('The game was a tie!')
+        print('The final score ' + str(self.p1.score) + ' TO ' +
+            str(self.p2.score))
+# This class starts the game prints information and calls the playround class
+# Also prints out the final score
+
+    def play_single(self):
+        print("Rock Paper Scissors, Go!")
+        print (f"Round 1 of 1:")
+        self.play_round()
         if self.p1.score > self.p2.score:
             print('Player 1 won!')
         elif self.p1.score < self.p2.score:
@@ -113,8 +123,6 @@ class Game():
             print('The game was a tie!')
         print('The final score ' + str(self.p1.score) + ' TO ' +
               str(self.p2.score))
-  ## This class starts the game prints information and calls the playround class
-  ## Also prints out the final score
 
     def play_round(self):
         move1 = self.p1.move()
@@ -122,9 +130,8 @@ class Game():
         result = Game.play(move1, move2)
         self.p1.learn(move2)
         self.p2.learn(move1)
-##This class calls the play class, sets the move 1 and 2 variables
-## then sets the result variable. Plus stores the players move
-
+# This class calls the play class, sets the move 1 and 2 variables
+# then sets the result variable. Plus stores the players move
 
     def play(self, move1, move2):
             print(f"You played {move1}")
@@ -143,24 +150,44 @@ class Game():
                 print ("** It's A TIE **")
                 print(f"Score: Player 1: {move1}  Player 2: {move2}\n\n")
                 return 0
-## This class calls the beats functions
+# This class calls the beats functions
 
 
 def beats(one, two):
     return ((one == 'rock' and two == 'scissors') or
             (one == 'scissors' and two == 'paper') or
             (one == 'paper' and two == 'rock'))
-## I believe beats would be considered a function and not a module because
-## it not part of a class...
-
-
-
-
-
+# I believe beats would be considered a function and not a module because
+# it not part of a class...
 
 
 if __name__ == '__main__':
-    #game = Game(Player(), Player())
-    #game.play_game()
-    Game = Game()
-    Game.play_game()
+    p2 = input('Type the selected Game to play: rock,\
+random, reflective, or cycles: >')
+    rounds = input('Enter 1 for a single game and 2 for a full game: >')
+
+    while p2 == 'rock'or p2 == 'random'or p2 == 'reflective' or p2 == 'cycles':
+
+        if p2 == 'rock':
+            p2 = Player()
+        elif p2 == 'random':
+            p2 = RandomPlayer()
+        elif p2 == 'cycles':
+            p2 = Cycles()
+        elif p2 == 'reflective':
+            p2 = ReflectPlayer()
+        else:
+            print('Sorry try again')
+            p2 = input('Select Game to Play: Rock, Random, Reflective, or Cycles')
+
+    Game = Game(p2)
+    while True:
+        if rounds == 'single':
+            Game.play_single()
+            break
+        elif rounds == 'full':
+            Game.play_game()
+            break
+        else:
+            print('Sorry try again')
+            rounds = input('Enter 1 for a single game and 2 for a full game: >')
